c9510aa42f48a68254a8bc5efe5fa1e00b4ea625
MatiasSingermann <101400498+MatiasSingermann@users.noreply.github.com>
2023-06-07 16:07:32 -0300

    useRef form send

--- src/components/InputLoginButton.tsx
+++ src/components/InputLoginButton.tsx
@@ -4,9 +4,10 @@ interface InputLoginButtonProps {
   icon: JSX.Element;
   type: string;
   pholder: string;
+  mlength: number;
 }
 
-function InputLoginButton({ icon, type, pholder }: InputLoginButtonProps) {
+function InputLoginButton({ icon, type, pholder, mlength }: InputLoginButtonProps) {
   const [invalid, setInvalid] = useState(true);
 
   return (
@@ -23,6 +24,8 @@ function InputLoginButton({ icon, type, pholder }: InputLoginButtonProps) {
         className="disabled mb-[12px] h-[52px] w-[310px] appearance-none rounded-[1.25rem] border-2 border-[#E8E8E8] bg-[#FBF9FA] pl-11 text-[20px] text-[#C4C4C4] shadow-md placeholder:text-[#C4C4C4] invalid:border-red-500 invalid:text-red-500 focus:border-[2.5px] focus:outline-none focus:placeholder:text-transparent invalid:focus:text-red-500"
         type={type}
         placeholder={pholder}
+        required
+        minLength={mlength}
       />
     </div>
   );
--- src/components/SignButton.tsx
+++ src/components/SignButton.tsx
@@ -3,9 +3,23 @@ interface SignButtonProps {
     text: string
 }  
 
+const handleSignClick = () => {
+  let obj = {
+    // email: InputLoginButton.value,
+    // password: InputLoginButton.value
+  }
+  fetch("/api", {
+    method: "POST",
+    headers: {
+      "Content-type":"application/json"
+    },
+    body:JSON.stringify(obj)
+  })
+}
+
 function SignButton({type, text} : SignButtonProps) {
   return (
-    <button className="bg-[#59C3C3] shadow-xl text-white text-[24px] rounded-3xl w-[192px] h-[38px] mt-[9px] font-coolveticaRegular" type={type}>{text}</button>
+    <button className="bg-[#59C3C3] shadow-xl text-white text-[24px] rounded-3xl w-[192px] h-[38px] mt-[9px] font-coolveticaRegular" type={type} onClick={handleSignClick}>{text}</button>
   )
 }
 
