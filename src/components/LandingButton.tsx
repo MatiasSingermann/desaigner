8ec30e4130d9d706d490ff7cb4a8808b437d190d
MatiasSingermann <matias.singermann@gmail.com>
2023-08-02 19:10:03 -0300

    landing page progress

---
+++ src/components/LandingButton.tsx
@@ -0,0 +1,20 @@
+import Link from "next/link";
+
+interface LandingButtonProps {
+  text: string;
+  color: string;
+  link: string;
+}
+
+function LandingButton({ text, color, link }: LandingButtonProps) {
+  return (
+    <Link
+      href={link}
+      className={`mb-[22px] flex h-[36px] w-[186px] cursor-pointer items-center justify-center rounded-2xl font-coolveticaRegular text-[20px] text-[#FBF9FA] shadow-xl ${color}`}
+    >
+      {text}
+    </Link>
+  );
+}
+
+export default LandingButton;
