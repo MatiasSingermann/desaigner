fc772dc730c153a6a32e4949043210924b6150e9
MatiasSingermann <matias.singermann@gmail.com>
2023-08-05 14:27:04 -0300

    show selected text

--- src/components/ComboBoxElement.tsx
+++ src/components/ComboBoxElement.tsx
@@ -1,15 +1,21 @@
+import { ChangeEvent, ChangeEventHandler, Dispatch, SetStateAction } from "react";
+
 interface ComboBoxElementProps {
   name: string;
   text: string;
+  value: string;
+  handleRadioChange: ChangeEventHandler<HTMLInputElement>;
 }
 
-function ComboBoxElement({ name, text }: ComboBoxElementProps) {
+function ComboBoxElement({ name, text, value, handleRadioChange }: ComboBoxElementProps) {
   return (
     <div className="relative flex h-[52px] w-[310px] items-center justify-start bg-[#292F2D] dark:bg-[#292F2D] 480:h-[58px] 480:w-[342px]">
       <input
         type="radio"
         name={name}
-        className="absolute hidden h-[52px] w-[310px] 480:h-[58px] 480:w-[342px]"
+        value={value}
+        onChange={handleRadioChange}
+        className="absolute ahidden h-[52px] w-[310px] 480:h-[58px] 480:w-[342px]"
       />
       <p className="mx-[24px] text-[20px] 480:text-[22px]">{text}</p>
     </div>
