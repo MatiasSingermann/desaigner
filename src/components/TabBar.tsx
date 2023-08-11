7705cc382068a126655e70d71b1ea3b9002ac940
MatiasSingermann <matias.singermann@gmail.com>
2023-08-11 20:24:14 -0300

    tabbar correction

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -5,17 +5,27 @@ import House from "./House";
 import Sparkle from "./Sparkle";
 import Gear from "./Gear";
 import Folder from "./Folder";
+import TabCurve from "./TabCurve";
 
 function TabBar() {
   const [selected, setSelected] = useState(false);
   return (
-    <div className="fixed bottom-0 z-[100] flex h-[70px] w-full items-center justify-between bg-[#228187] px-[24px] 480:hidden">
-      <TabBarButton href="/landing" image={<Rocket />} />
-      <TabBarButton href="/home" image={<House />} />
-      <TabBarButton href="/create" image={<Sparkle />} />
-      <TabBarButton href="/collections" image={<Folder />} />
-      <TabBarButton href="/settings" image={<Gear />} />
-    </div>
+    <>
+      <div className="relative flex h-[70px] w-full">
+        <div className="relative -top-[45px] flex items-center justify-center">
+          <TabCurve />
+        </div>
+      </div>
+      <div className="fixed bottom-0 z-[100] flex h-[70px] w-full items-center justify-center bg-[#228187] px-[24px] 480:hidden">
+        <ul className="flex items-center justify-between h-full w-full">
+          <TabBarButton href="/landing" image={<Rocket />} />
+          <TabBarButton href="/home" image={<House />} />
+          <TabBarButton href="/create" image={<Sparkle />} />
+          <TabBarButton href="/collections" image={<Folder />} />
+          <TabBarButton href="/settings" image={<Gear />} />
+        </ul>
+      </div>
+    </>
   );
 }
 
--- src/components/TabBarButton.tsx
+++ src/components/TabBarButton.tsx
@@ -1,17 +1,20 @@
 import Link from "next/link";
 
 interface TabBarButtonProps {
-    href: string;
-    image: JSX.Element;
+  href: string;
+  image: JSX.Element;
 }
 
-function TabBarButton({href, image} : TabBarButtonProps) {
+function TabBarButton({ href, image }: TabBarButtonProps) {
   return (
-    <Link href={href}>
-      <div className="flex h-[50px] w-[50px] items-center justify-center rounded-full bg-[#59C3C3]">
-        {image}
-      </div>
-    </Link>
+    <li>
+      {" "}
+      <Link href={href}>
+        <div className="flex h-[50px] w-[50px] items-center justify-center rounded-full bg-[#59C3C3]">
+          {image}
+        </div>
+      </Link>
+    </li>
   );
 }
 
