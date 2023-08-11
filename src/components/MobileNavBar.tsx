0d7bf70efa5cf9196230f0bdd4b995e0c5b93e30
MatiasSingermann <matias.singermann@gmail.com>
2023-08-11 19:32:29 -0300

    header fix

--- src/components/MobileNavBar.tsx
+++ src/components/MobileNavBar.tsx
@@ -1,16 +1,22 @@
-import DesAIgnerLogo from "./DesAIgnerLogo"
+import DesAIgnerLogo from "./DesAIgnerLogo";
 import Link from "next/link";
+import { useRouter } from "next/router";
 
 function MobileNavBar() {
+  const router = useRouter();
   return (
-    <header className="flex justify-center items-center top-0 w-full h-[140px] 480:h-[180px]">
-        <nav className="480:scale-[1.4]">
-          <Link href="/landing" aria-label="Vuelve a Landing">
-            <DesAIgnerLogo/>
-          </Link>
-        </nav>
+    <header
+      className={`top-0 mb-[18px] flex w-full items-center border-b-[2px] border-[#D9D9D9] p-[24px] dark:border-black ${
+        router.route == "/login" ? "justify-center" : ""
+      }`}
+    >
+      <nav className="480:scale-[1.4]">
+        <Link href="/landing" aria-label="Vuelve a Landing">
+          <DesAIgnerLogo />
+        </Link>
+      </nav>
     </header>
-  )
+  );
 }
 
-export default MobileNavBar
+export default MobileNavBar;
