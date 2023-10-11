8902e5235844a404b50822c93681d1a42ab76be7
MatiasSingermann <101400498+MatiasSingermann@users.noreply.github.com>
2023-10-11 12:06:16 -0300

    working on tabbar

--- src/components/TabBar.tsx
+++ src/components/TabBar.tsx
@@ -20,41 +20,41 @@ const TabBar = memo(function TabBar() {
   const myRoute = router.route;
 
   const handleClick = (e: React.MouseEvent<HTMLDivElement>) => {
-    const targetDiv = e.currentTarget;
-    const targetRect = targetDiv.getBoundingClientRect();
-    const targetRectX = targetRect.x;
+    // const targetDiv = e.currentTarget;
+    // const targetRect = targetDiv.getBoundingClientRect();
+    // const targetRectX = targetRect.x;
    
-    if(targetDiv != previousDiv){
-      if(targetDiv.className.includes("tabbar-selected") === false){
-        // targetDiv.className += " tabbar-selected";
-      }
-      if(counter === 1){
-        previousDiv!.className = previousDivClassName;
-      }
-      setPreviousDiv(targetDiv);
-      setPreviousDivClassName("flex h-[50px] w-[50px] items-center justify-center rounded-full transition-all")
-    }
+    // if(targetDiv != previousDiv){
+    //   if(targetDiv.className.includes("tabbar-selected") === false){
+    //     // targetDiv.className += " tabbar-selected";
+    //   }
+    //   if(counter === 1){
+    //     previousDiv!.className = previousDivClassName;
+    //   }
+    //   setPreviousDiv(targetDiv);
+    //   setPreviousDivClassName("flex h-[50px] w-[50px] items-center justify-center rounded-full transition-all")
+    // }
 
-    if(counter == 0) {
-      setCounter(1);
-    }
+    // if(counter == 0) {
+    //   setCounter(1);
+    // }
 
-    if (elementRef.current != element) {
-      element = elementRef.current;
-    }
+    // if (elementRef.current != element) {
+    //   element = elementRef.current;
+    // }
 
-    const width = element!.offsetWidth;
-    setTranslateX(Math.floor(targetRectX - width / 4.8));
+    // const width = element!.offsetWidth;
+    // setTranslateX(Math.floor(targetRectX - width / 4.8));
   };
   return (
     <div className="fixed bottom-0 z-[100] flex h-[70px] w-full 480:hidden">
-      <div
+      {/* <div
         className="absolute bottom-[66px] flex items-center justify-center transition-all"
         ref={elementRef}
         style={{ translate: translateX }}
       >
         <TabCurve />
-      </div>
+      </div> */}
       <div className="flex h-[70px] w-full items-center justify-center bg-[#228187] px-[24px]">
         <ul className="flex h-full w-full items-center justify-between">
           <TabBarButton
--- src/components/TabBarButton.tsx
+++ src/components/TabBarButton.tsx
@@ -1,5 +1,6 @@
 import Link from "next/link";
 import type { MouseEventHandler } from "react";
+import TabCurve from "./TabCurve";
 
 interface TabBarButtonProps {
   name: string;
@@ -17,22 +18,49 @@ function TabBarButton({
   myRoute,
 }: TabBarButtonProps) {
   const liContent = () => {
-    const rocketCondition = name === "Rocket" && (myRoute === "/landing" || myRoute === "/login") ? "tabbar-selected" : "";
-    const homeCondition = name === "House" && myRoute === "/home" ? "tabbar-selected" : "";
-    const sparkleCondition = name === "Sparkle" && myRoute === "/create" ? "tabbar-selected" : "";
-    const folderCondition = name === "Folder" && myRoute === "/collections" ? "tabbar-selected" : "";
-    const gearCondition = name === "Gear" && myRoute === "/settings" ? "tabbar-selected" : "";
+    const rocketCondition =
+      (name === "Rocket" && (myRoute === "/landing" || myRoute === "/login"))
+        ? "tabbar-selected"
+        : "";
+    const homeCondition =
+      (name === "House" && myRoute === "/home") ? "tabbar-selected" : "";
+    const sparkleCondition =
+      (name === "Sparkle" && myRoute === "/create") ? "tabbar-selected" : "";
+    const folderCondition =
+      (name === "Folder" && myRoute === "/collections") ? "tabbar-selected" : "";
+    const gearCondition =
+      (name === "Gear" && myRoute === "/settings") ? "tabbar-selected" : "";
+
+    const curveRocketCondition =
+      (name === "Rocket" && (myRoute === "/landing" || myRoute === "/login"))
+        ? "flex"
+        : "hidden";
+    const curveHomeCondition =
+      (name === "House" && myRoute === "/home") ? "flex" : "hidden";
+    const curveSparkleCondition =
+      (name === "Sparkle" && myRoute === "/create") ? "flex" : "hidden";
+    const curveFolderCondition =
+      (name === "Folder" && myRoute === "/collections") ? "flex" : "hidden";
+    const curveGearCondition =
+      (name === "Gear" && myRoute === "/settings") ? "flex" : "hidden";
     return (
-      <li>
-        <Link href={href} aria-label={"Ir a" + href}>
-          <div
-            className={`flex h-[50px] w-[50px] items-center justify-center rounded-full transition-all ${name} ${rocketCondition} ${homeCondition} ${sparkleCondition} ${folderCondition} ${gearCondition}`}
-            onClick={handleClick}
-          >
-            {image}
-          </div>
-        </Link>
-      </li>
+      <div className="flex flex-col items-center">
+        <div
+          className={`absolute -top-[24px] mb-[80px] ${curveRocketCondition} ${curveHomeCondition} ${curveSparkleCondition} ${curveFolderCondition} ${curveGearCondition}`}
+        >
+          <TabCurve />
+        </div>
+        <li>
+          <Link href={href} aria-label={"Ir a" + href}>
+            <div
+              className={`z-10 flex h-[50px] w-[50px] items-center justify-center rounded-full transition-all ${rocketCondition} ${homeCondition} ${sparkleCondition} ${folderCondition} ${gearCondition}`}
+              onClick={handleClick}
+            >
+              {image}
+            </div>
+          </Link>
+        </li>
+      </div>
     );
   };
   return (
