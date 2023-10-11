68468884ae86dbc5da699e15de38b9b9c4d7e0c8
MatiasSingermann <101400498+MatiasSingermann@users.noreply.github.com>
2023-10-11 10:05:35 -0300

    save image & info progress

--- src/components/SaveImageInfo.tsx
+++ src/components/SaveImageInfo.tsx
@@ -1,5 +1,5 @@
 import type { FullDataImage } from "~/hooks/useImageData";
-import base64 from 'base64-js'
+import base64 from "base64-js";
 
 interface SaveImageInfo {
   environment: string;
@@ -9,9 +9,25 @@ interface SaveImageInfo {
   furniture: FullDataImage;
 }
 
-function SaveImageInfo({environment, budget, style, image, furniture}: SaveImageInfo) {
+function SaveImageInfo({
+  environment,
+  budget,
+  style,
+  image,
+  furniture,
+}: SaveImageInfo) {
   const handleSubmit = (e: React.ChangeEvent<HTMLFormElement>) => {
     e.preventDefault();
+
+    let base64String = "";
+
+    const reader = new FileReader();
+    reader.onloadend = () => {
+      const arrayBuffer = reader.result as ArrayBuffer;
+      const byteArray = new Uint8Array(arrayBuffer);
+      base64String = base64.fromByteArray(byteArray);
+    };
+    reader.readAsArrayBuffer(image);
 
     const formData = new FormData(e.currentTarget);
     const inputData = [];
@@ -19,34 +35,37 @@ function SaveImageInfo({environment, budget, style, image, furniture}: SaveImage
     for (const pair of formData.entries()) {
       inputData.push(pair);
     }
-    
-    const nombre = inputData[0]?inputData[0][1]:"";
-    const coleccion = inputData[1]?inputData[1][1]:"";
+
+    const nombre = inputData[0] ? inputData[0][1] : "";
+    const coleccion = inputData[1] ? inputData[1][1] : "";
 
     const obj = {
       nombre: nombre,
       ambiente: environment,
-      // disenioIMG: base64.fromByteArray(image),
+      disenioIMG: base64String,
       muebles: furniture,
       presupuesto: budget,
       estilo: style,
       colecciones: coleccion,
     };
 
-    fetch("http://localhost:8000/img2img/v3", {
+    fetch("api/auth/createDisenio", {
       method: "POST",
       body: JSON.stringify(obj),
     })
       .then((response) => response.json())
       .then((data) => {
-        // a
+        console.log(data);
       })
       .catch((error: Error) => {
-        // a
+        console.log(error);
       });
-  }
+  };
   return (
-    <form onSubmit={handleSubmit} className="bg-red flex h-full w-full items-center justify-center">
+    <form
+      onSubmit={handleSubmit}
+      className="bg-red flex h-full w-full items-center justify-center rounded-xl"
+    >
       <input name="Nombre" type="text" />
       <input name="Colecciones" type="text" />
     </form>
