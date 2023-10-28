8c1188c53ddb6c052564a1b556ca82661b565afc
MatiasSingermann <matias.singermann@gmail.com>
2023-10-28 18:59:59 -0300

    change url

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -59,7 +59,7 @@ export const authOptions = {
                     email: string;
                     contrasenia: string;
                 };
-                const res = await fetch("http://localhost:3000/api/auth/login", {
+                const res = await fetch("https://desaigner.vercel.app/api/auth/login", {
                     method: "POST",
                     headers: {
                         "Content-Type": "application/json"
